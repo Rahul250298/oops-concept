795824aefe2c27f9eb10c052a774163b1d2e65ce
Rahul250298 <91786240+Rahul250298@users.noreply.github.com>
2022-01-21 18:11:39 +0530

    Added virtual and override  to get desired output

--- oops concept/oops concept/polymorphism/Class1.cs
+++ oops concept/oops concept/polymorphism/Class1.cs
@@ -8,35 +8,35 @@ namespace oops_concept.polymorphism
 {
     public class Country
     {
-        public void special()
+        public virtual void special()
         {
             Console.WriteLine("Humans look beauty in their way");
         }
     }
     class India : Country
     {
-         public void special()
+         public override void special()
         {
             Console.WriteLine("population");
         }
     }
     class Us: Country
     {
-        public void special()
+        public override void special()
         {
             Console.WriteLine("Tech Giants");
         }
     }
     class  China : Country
     {
-        public void special()
+        public override void special()
         {
             Console.WriteLine("Super Power");
         }
     }
     class Newzealand:Country
     {
-        public void special()
+        public override void special()
         {
             Console.WriteLine("Womens Safety");
 
